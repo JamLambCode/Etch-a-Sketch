a52c7fa878ebbbe525d5e8e56a3cfe4c675a3f2f
Jakob Lamber <mr.jamlamb@gmail.com>
2022-11-27 11:37:07 -0700

    Making sketcher have fluid width/height and grid amt

--- JavaScript.js
+++ JavaScript.js
@@ -1,12 +1,28 @@
 const container = document.querySelector(".container");
 
-const pixel = document.createElement("div");
+function createPixels(num){
+    for(let j = 0; j< num; j++){   
+        let row = document.createElement("div"); 
+        row.classList.add('row');
+        for(let i = 0;i<num;i++){
+            let pixel = document.createElement("div");
+            pixel.classList.add("pixel");
+
+            pixel.addEventListener("mouseover", function(){
+                pixel.classList.add("colorChange");
+            });
+
+            row.appendChild(pixel);
+        }
+        container.appendChild(row);
+    }
+    
+}
+
+
+
 //here's the div that we are gonna work w/ and we give it a class to make ti sketch
-pixel.classList.add("pixel");
 
-container.appendChild(pixel);
 
-pixel.addEventListener("mouseover", function(){
-    pixel.classList.add("colorChange");
-});
+createPixels(6);
 

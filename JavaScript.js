9570ad7b79b9dbd65dddea361f4655171753479b
Jakob Lamber <mr.jamlamb@gmail.com>
2022-11-27 14:10:09 -0700

    Making functionality so that user can change the screen size

--- JavaScript.js
+++ JavaScript.js
@@ -1,20 +1,35 @@
+
+let empty = true;
+//this is a boolean value used to see if we need to reset the canvas if the screen resizes
+
 const container = document.querySelector(".container");
 
+
 function createPixels(num){
-    for(let j = 0; j< num; j++){   
-        let row = document.createElement("div"); 
-        row.classList.add('row');
-        for(let i = 0;i<num;i++){
-            let pixel = document.createElement("div");
-            pixel.classList.add("pixel");
+    if(empty == false){
+        while (container.hasChildNodes()) {
+            container.removeChild(container.firstChild);
+          }
+          empty = true;
+        createPixels(num);
+    }
+    else{
+        for(let j = 0; j< num; j++){   
+            let row = document.createElement("div"); 
+            row.classList.add('row');
+            for(let i = 0;i<num;i++){
+                let pixel = document.createElement("div");
+                pixel.classList.add("pixel");
 
-            pixel.addEventListener("mouseover", function(){
-                pixel.classList.add("colorChange");
-            });
+                pixel.addEventListener("mouseover", function(){
+                    pixel.classList.add("colorChange");
+                });
 
-            row.appendChild(pixel);
+                row.appendChild(pixel);
+            }
+            container.appendChild(row);
         }
-        container.appendChild(row);
+        empty = false;
     }
     
 }
@@ -24,10 +39,14 @@ function resetColor(){
     //needs to be given a container with stuff in it
 }
 
+const btns = document.querySelectorAll(".btn");
+btns.forEach(btn => btn.addEventListener('click', function(){
+    createPixels(btn.value);
+}));
+
 
 
 //here's the div that we are gonna work w/ and we give it a class to make ti sketch
 
 
-createPixels(64);
 
